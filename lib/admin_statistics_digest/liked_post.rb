7e940d58da2fff955ade74220dda3dcc36c861f5
M Saiqul Haq <saiqulhaq@gmail.com>
2016-09-07 20:29:46 +0700

    Add LikedPost report

---
+++ lib/admin_statistics_digest/liked_post.rb
@@ -0,0 +1,20 @@
+module AdminStatisticsDigest
+end
+
+require_relative '../admin_statistics_digest/base_report'
+
+class AdminStatisticsDigest::LikedPost < AdminStatisticsDigest::BaseReport
+  provide_filter :between
+  provide_filter :limit
+
+  def to_sql
+    <<~SQL
+SELECT "posts".* FROM "posts"
+WHERE ("posts"."deleted_at" IS NULL)
+#{"AND ((\"posts\".\"created_at\", \"posts\".\"created_at\") OVERLAPS ('#{filters.date_range.first.beginning_of_day}', '#{filters.date_range.last.end_of_day}') OR DATE(\"posts\".\"created_at\") = '#{filters.date_range.first}' OR DATE(\"posts\".\"created_at\") =  '#{filters.date_range.last}')" if filters.date_range }
+ORDER BY like_count DESC
+#{"LIMIT #{filters.limit}" if filters.limit }
+    SQL
+  end
+
+end
--- spec/lib/admin_statistics_digest/liked_post_spec.rb
+++ spec/lib/admin_statistics_digest/liked_post_spec.rb
@@ -1,20 +1,95 @@
-require_relative '../../spec/spec_helper'
-require_relative '../../lib/admin_statistics_digest/liked_post'
+require_relative '../../../spec/spec_helper'
+require_relative '../../../lib/admin_statistics_digest/liked_post'
 
 RSpec.describe AdminStatisticsDigest::LikedPost do
-  before { Post.delete_all }
-  let(:user) { User.last || Fabricate.create(:user) }
-  let(:topic) { Topic.last || Fabricate.create(:topic) }
-  let!(:p1) { create_post(like_count: 100, user: user, topic: topic) }
-  let!(:p2) { create_post(like_count: 50, user: user, topic: topic) }
-  let!(:p3) { create_post(like_count: 150, user: user, topic: topic) }
-  let!(:p4) { create_post(like_count: 30, user: user, topic: topic) }
-  let!(:p5) { create_post(like_count: 5, user: user, topic: topic) }
+  let(:report) { described_class.new }
 
-  it 'get all posts and ordered the results by like' do
-    posts = described_class.new
-    result = exec_sql(posts.to_sql)
-    expect(result.map {|p| p['id'].to_i }).to match_array([p3.id, p1.id, p2.id, p4.id, p5.id])
+  before(:all) { Post.delete_all }
+
+  Timecop.freeze 1.year.ago do
+    let(:user) { User.last || Fabricate.create(:user) }
+    let(:topic) { Topic.last || Fabricate.create(:topic) }
+  end
+
+  let!(:post_at_1_month_ago) do
+    Timecop.freeze 1.month.ago do
+      create_post(user: user, topic: topic).tap {|p| p.update_column(:like_count, 100) }
+    end
+  end
+
+  let!(:post_at_2_month_ago) do
+    Timecop.freeze 2.months.ago do
+      create_post(user: user, topic: topic).tap {|p| p.update_column(:like_count, 50) }
+    end
+  end
+
+  let!(:post_at_3_month_ago) do
+    Timecop.freeze 3.months.ago do
+      create_post(user: user, topic: topic).tap {|p| p.update_column(:like_count, 150) }
+    end
+  end
+
+  let!(:post_at_4_month_ago) do
+    Timecop.freeze 4.months.ago do
+      create_post(user: user, topic: topic).tap {|p| p.update_column(:like_count, 30) }
+    end
+  end
+
+  let!(:post_at_5_month_ago) do
+    Timecop.freeze 5.months.ago do
+      create_post(user: user, topic: topic).tap {|p| p.update_column(:like_count, 5) }
+    end
+  end
+
+
+  describe '#to_sql' do
+
+    it 'returns SQL query to get all posts and ordered the results by counting "post.like" ' do
+      result = exec_sql(report.to_sql)
+      expect(result.map {|p| p['id'].to_i }).to(
+        match_array([
+                      post_at_3_month_ago.id,
+                      post_at_1_month_ago.id,
+                      post_at_2_month_ago.id,
+                      post_at_4_month_ago.id,
+                      post_at_5_month_ago.id
+                    ]))
+    end
+  end
+
+  it 'has :between, and :limit filters' do
+    expect(subject.available_filters).to match_array([:between, :limit])
+  end
+
+  describe ':between filter' do
+    it 'filtering post#created_at' do
+      report.filters do
+        between 3.months.ago..2.months.ago
+      end
+
+      result = exec_sql(report.to_sql)
+      expect(result.map {|p| p['id'].to_i }).to(
+        match_array([
+                      post_at_3_month_ago.id,
+                      post_at_2_month_ago.id
+                    ]))
+    end
+  end
+
+  describe ':limit filter' do
+    it 'limits the result' do
+      report.filters do
+        limit 3
+      end
+
+      result = exec_sql(report.to_sql)
+      expect(result.map {|p| p['id'].to_i }).to(
+        match_array([
+                      post_at_3_month_ago.id,
+                      post_at_2_month_ago.id,
+                      post_at_1_month_ago.id
+                    ]))
+    end
   end
 
 end
