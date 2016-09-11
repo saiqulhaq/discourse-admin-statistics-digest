930096cc4b9916a46f4d8380f80c8cd645e76030
M Saiqul Haq <saiqulhaq@gmail.com>
2016-09-11 14:54:00 +0700

    Add more spec for PopularTopic

--- spec/lib/admin_statistics_digest/popular_topic_spec.rb
+++ spec/lib/admin_statistics_digest/popular_topic_spec.rb
@@ -131,6 +131,55 @@ RSpec.describe AdminStatisticsDigest::PopularTopic do
     @topic_5
   end
 
+  let!(:excluded_topic_with_score_15) do
+    Timecop.freeze 2.month.ago.end_of_month do
+
+      Timecop.freeze 20.days.ago do
+        @topic_6 = create_topic user: users.sample
+        5.times { create_post topic: @topic_6, user_id: users.sample.id }
+      end
+
+      Timecop.freeze 10.days.ago do
+        5.times { create_post topic: @topic_6, user: users.sample }
+      end
+
+      Timecop.freeze 17.days.ago do
+        5.times { create_post topic: @topic_6, user: users.sample }
+      end
+
+    end
+
+    @topic_6.first_post.update_attribute :like_count, 15
+    @topic_6.update_attributes views: 15, like_count: 15, posts_count: @topic_6.posts.count
+    @topic_6
+  end
+
+  let!(:excluded_topic_with_score_18) do
+    Timecop.freeze Date.today.end_of_month do
+
+      Timecop.freeze 20.days.ago do
+        @topic_7 = create_topic user: users.sample
+        5.times { create_post topic: @topic_7, user_id: users.sample.id }
+      end
+
+      Timecop.freeze 10.days.ago do
+        5.times { create_post topic: @topic_7, user: users.sample }
+      end
+
+      Timecop.freeze 17.days.ago do
+        3.times { create_post topic: @topic_7, user: users.sample }
+      end
+
+      Timecop.freeze 5.days.ago do
+        5.times { create_post topic: @topic_7, user_id: users.sample.id }
+      end
+    end
+
+    @topic_7.first_post.update_attribute :like_count, 18
+    @topic_7.update_attributes views: 18, like_count: 18, posts_count: @topic_7.posts.count
+    @topic_7
+  end
+
   describe 'test_fixtures' do
     it 'topic_score_10 should be correct' do
       expect(topic_score_10.views).to eq(10)
@@ -152,12 +201,14 @@ RSpec.describe AdminStatisticsDigest::PopularTopic do
       expect(topic_score_12.like_count).to eq(12)
       expect(topic_score_12.first_post.like_count).to eq(12)
     end
+
     it 'topic_score_4 should be correct' do
       expect(topic_score_4.views).to eq(4)
       expect(topic_score_4.posts_count).to eq(4)
       expect(topic_score_4.like_count).to eq(4)
       expect(topic_score_4.first_post.like_count).to eq(4)
     end
+
     it 'topic_score_5 should be correct' do
       expect(topic_score_5.views).to eq(5)
       expect(topic_score_5.posts_count).to eq(5)
@@ -165,6 +216,19 @@ RSpec.describe AdminStatisticsDigest::PopularTopic do
       expect(topic_score_5.first_post.like_count).to eq(5)
     end
 
+    it 'excluded_topic_with_score_15 should be correct' do
+      expect(excluded_topic_with_score_15.views).to eq(15)
+      expect(excluded_topic_with_score_15.posts_count).to eq(15)
+      expect(excluded_topic_with_score_15.like_count).to eq(15)
+      expect(excluded_topic_with_score_15.first_post.like_count).to eq(15)
+    end
+
+    it 'excluded_topic_with_score_18 should be correct' do
+      expect(excluded_topic_with_score_18.views).to eq(18)
+      expect(excluded_topic_with_score_18.posts_count).to eq(18)
+      expect(excluded_topic_with_score_18.like_count).to eq(18)
+      expect(excluded_topic_with_score_18.first_post.like_count).to eq(18)
+    end
   end
 
   it 'generate top topic when initialized' do
